a9f4f1f55204ca80ef45ee8db2e48dc0358bcedd
Vinay Sama <vinay.sama@qentelli.com>
2024-11-13 14:58:56 +0530

    fix: Home page changes

--- src/pages/demo/demo.component.ts
+++ src/pages/demo/demo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {BookConsultationComponent} from '../../common/sharedComponents/book-consultation/book-consultation.component';
 import {TransformCustomerServiceComponent} from '../../common/sharedComponents/transform-customer-service/transform-customer-service.component';
+import {ContactUsComponent} from '../../common/sharedComponents/contact-us/contact-us.component'
 @Component({
   selector: 'app-demo',
   standalone: true,
-  imports: [BookConsultationComponent, TransformCustomerServiceComponent ],
+  imports: [BookConsultationComponent, TransformCustomerServiceComponent, ContactUsComponent ],
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.scss'
 })
