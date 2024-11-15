86340d521c2fc6daccefd39b6c1979abd23bbaba
Suresh Merugu <smerugu@brierley.com>
2024-11-15 15:47:20 +0530

    platform screen added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './../pages/home/home.component';
 import { AboutComponent } from '../pages/about/about.component';
 import { DemoComponent } from '../pages/demo/demo.component';
-import { SelfcheckoutComponent } from '../pages/selfcheckout/selfcheckout.component'
+import { SelfcheckoutComponent } from '../pages/selfcheckout/selfcheckout.component';
+import { PlatformComponent } from '../pages/platform/platform.component';
 
 export const routes: Routes = [
   { path: 'home', pathMatch: 'full', component: HomeComponent },
   { path: 'about', pathMatch: 'full', component: AboutComponent },
   { path: 'demo', pathMatch: 'full', component: DemoComponent },
-  { path: 'self-checkout', pathMatch: 'full', component: SelfcheckoutComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  {
+    path: 'self-checkout',
+    pathMatch: 'full',
+    component: SelfcheckoutComponent,
+  },
+  { path: 'platform', pathMatch: 'full', component: PlatformComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
