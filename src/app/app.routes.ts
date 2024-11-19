c7a00cb1354f797d3a7e8b4e9e05a82ba5245db9
Suresh Merugu <smerugu@brierley.com>
2024-11-19 18:42:20 +0530

    solutions page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { DemoComponent } from '../pages/demo/demo.component';
 import { SelfcheckoutComponent } from '../pages/selfcheckout/selfcheckout.component';
 import { PlatformComponent } from '../pages/platform/platform.component';
 import { SupportComponent } from '../pages/support/support.component';
+import { SolutionsComponent } from '../pages/solutions/solutions.component';
 
 export const routes: Routes = [
   { path: 'home', pathMatch: 'full', component: HomeComponent },
@@ -17,5 +18,6 @@ export const routes: Routes = [
   },
   { path: 'platform', pathMatch: 'full', component: PlatformComponent },
   { path: 'support', pathMatch: 'full', component: SupportComponent },
+  { path: 'solutions', pathMatch: 'full', component: SolutionsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
