ffacbbbaa7ff64504b3b7fafe68e7c02f0bd7e6d
Vinay Sama <vinay.sama@qentelli.com>
2024-11-21 16:01:22 +0530

    fix: Book a demo page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from '../pages/about/about.component';
 import { DemoComponent } from '../pages/demo/demo.component';
 import { SelfcheckoutComponent } from '../pages/selfcheckout/selfcheckout.component';
 import { PlatformComponent } from '../pages/platform/platform.component';
-
+import {BookademoComponent} from '../pages/bookademo/bookademo.component';
 export const routes: Routes = [
   { path: 'home', pathMatch: 'full', component: HomeComponent },
   { path: 'about', pathMatch: 'full', component: AboutComponent },
@@ -15,5 +15,6 @@ export const routes: Routes = [
     component: SelfcheckoutComponent,
   },
   { path: 'platform', pathMatch: 'full', component: PlatformComponent },
+  { path: 'bookademo', pathMatch: 'full', component: BookademoComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
