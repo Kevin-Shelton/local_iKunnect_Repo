f6e97be47915941ce446b9ab219f7d830c4dd0cb
Suresh Merugu <smerugu@brierley.com>
2024-11-21 18:26:56 +0530

    aboutus page added

--- src/pages/about/about.component.ts
+++ src/pages/about/about.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ContactUsComponent } from '../../common/sharedComponents/contact-us/contact-us.component';
+import { BookConsultationComponent } from '../../common/sharedComponents/book-consultation/book-consultation.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ContactUsComponent, BookConsultationComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  title: string = 'Powering Exceptional \n Customer Journey.';
 }
