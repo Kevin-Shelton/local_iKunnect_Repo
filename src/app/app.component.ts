630ea74f23963c188dbc59697b34736087b2a0a3
Suresh Merugu <smerugu@brierley.com>
2024-11-29 15:15:59 +0530

    header navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import * as AOS from 'aos';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from '../common/app-header/app-header.component';
 import { AppFooterComponent } from '../common/app-footer/app-footer.component';
 import { isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AppHeaderComponent,AppFooterComponent],
+  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'konnect-invictus';
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       let element: any = document?.getElementsByClassName('zammad-chat');
@@ -22,5 +22,10 @@ export class AppComponent implements OnInit{
         element[0].style.display = 'none';
       }
     }
+  }
+  ngAfterViewInit() {
+    AOS.init({
+      once: true, // Animation will happen only once
+    });
   }
 }
