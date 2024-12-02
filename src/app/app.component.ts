b46c8f9e4c060f12320894f2d940bb731a0816c6
Vinay Sama <vinay.sama@qentelli.com>
2024-12-02 13:00:32 +0530

    fix: Added self checkout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from '../common/app-header/app-header.component';
 import { AppFooterComponent } from '../common/app-footer/app-footer.component';
 import { isPlatformBrowser } from '@angular/common';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -22,5 +23,10 @@ export class AppComponent implements OnInit{
         element[0].style.display = 'none';
       }
     }
+
+    AOS.init({
+      duration: 1200, // Animation duration
+      // once: true      // Whether animation should happen only once
+    });
   }
 }
