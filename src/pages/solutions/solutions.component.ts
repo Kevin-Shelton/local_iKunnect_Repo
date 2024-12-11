21974f7c55ebee7117c205dd2c4706e60b863feb
sowjanya balumuri <sowjanya.balumuri@qentelli.com>
2024-12-11 11:59:18 +0530

    solution page chat

--- src/pages/solutions/solutions.component.ts
+++ src/pages/solutions/solutions.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { TransformCustomerServiceComponent } from '../../common/sharedComponents/transform-customer-service/transform-customer-service.component';
 import { BookConsultationComponent } from '../../common/sharedComponents/book-consultation/book-consultation.component';
 import { HerosData } from '../../config/heros';
+import { LetsChatComponent } from '../../common/sharedComponents/lets-chat/lets-chat.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-solutions',
   standalone: true,
-  imports: [TransformCustomerServiceComponent, BookConsultationComponent],
+  imports: [TransformCustomerServiceComponent, BookConsultationComponent, LetsChatComponent, CommonModule],
   templateUrl: './solutions.component.html',
   styleUrl: './solutions.component.scss',
 })
 export class SolutionsComponent {
+  isChatEnabled: boolean = false;
   title: string = 'Powering Exceptional \n Customer Journeys.';
   herosConfig= HerosData.solution;
 }
