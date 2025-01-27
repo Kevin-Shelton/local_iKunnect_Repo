2c6200a98376fe83b094077d1cd6c695225a8b12
@satya.kancherla <satya.kancherla@qentelli.com>
2025-01-27 21:35:18 +0530

    appropriate message when agents are unavailable

--- src/common/sharedComponents/lets-chat/lets-chat.component.ts
+++ src/common/sharedComponents/lets-chat/lets-chat.component.ts
@@ -1,5 +1,12 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-lets-chat',
@@ -8,20 +15,65 @@ import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
   templateUrl: './lets-chat.component.html',
   styleUrl: './lets-chat.component.scss',
 })
-export class LetsChatComponent implements OnInit {
-  constructor(@Inject(PLATFORM_ID) private readonly platformId: object) {}
+export class LetsChatComponent implements OnInit, OnDestroy {
+  // interval!: NodeJS.Timeout
+  private mutationObserver: MutationObserver | null = null;
+  constructor(
+    @Inject(PLATFORM_ID) private readonly platformId: object,
+    private readonly renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId) && typeof document !== 'undefined') {
       setTimeout(() => {
         // eslint-disable-next-line
         const element: any = document?.getElementsByClassName('zammad-chat');
-        console.log('on init zammad element :::::::::::::::: ',element)
+        console.log('on init zammad element :::::::::::::::: ', element);
         if (element?.length) {
           element[0].style.display = 'block';
           element[0].className = 'zammad-chat chat-client';
+          this.setMessage();
         }
       }, 500);
+    }
+  }
+
+  setMessage() {
+    this.renderer.listen('document', 'click', (event: any) => {
+      const targetClasses = event?.target?.classList;
+      if (
+        targetClasses?.contains('zammad-chat-welcome') ||
+        targetClasses?.contains('zammad-chat-header') ||
+        targetClasses?.contains('zammad-chat-welcome-text')
+      ) {
+        this.monitorChatModal();
+      }
+    });
+  }
+
+  monitorChatModal() {
+    const chatContainer = document.querySelector('.zammad-chat-modal');
+    if (!chatContainer) return;
+
+    this.mutationObserver = new MutationObserver(() => {
+      const domElement: any = document.querySelector('.zammad-chat-modal-text');
+      console.log('domElement', domElement);
+      if (domElement && domElement.innerText.includes('colleagues are busy')) {
+        domElement.innerText =
+          'All our agents are currently busy, will serve you at the earliest. Please be patient or try again later.';
+        this.mutationObserver?.disconnect();
+      }
+    });
+
+    this.mutationObserver.observe(chatContainer, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.mutationObserver) {
+      this.mutationObserver.disconnect();
     }
   }
 }
