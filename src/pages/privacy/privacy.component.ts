f65178505471b3677c304ffefe5d0d66df5edb7d
Suresh Merugu <smerugu@brierley.com>
2025-03-07 14:37:28 +0530

    privacy and terms pages design added

--- src/pages/privacy/privacy.component.ts
+++ src/pages/privacy/privacy.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { BookConsultationComponent } from '../../common/sharedComponents/book-consultation/book-consultation.component';
 import { PdfReaderComponent } from '../../common/sharedComponents/pdf-reader/pdf-reader.component';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [PdfReaderComponent],
+  imports: [PdfReaderComponent, BookConsultationComponent],
   templateUrl: './privacy.component.html',
-  styleUrl: './privacy.component.scss'
+  styleUrl: './privacy.component.scss',
 })
 export class PrivacyComponent {
-
+  title: string = 'Powering Exceptional \n Customer Journeys.';
 }
--- src/pages/terms/terms.component.ts
+++ src/pages/terms/terms.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { BookConsultationComponent } from '../../common/sharedComponents/book-consultation/book-consultation.component';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
-  imports: [],
+  imports: [BookConsultationComponent],
   templateUrl: './terms.component.html',
-  styleUrl: './terms.component.scss'
+  styleUrl: './terms.component.scss',
 })
 export class TermsComponent {
-
+  title: string = 'Powering Exceptional \n Customer Journeys.';
 }
